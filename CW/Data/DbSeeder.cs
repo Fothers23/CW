4f176c2e80beab1c3015cfe2d5a765ad2d5a5d39
Emily Fothergill <emily.fothergill@btopenworld.com>
2019-01-07 19:34:14 +0000

    Removed old seeding stuff

--- CW/Data/DbSeeder.cs
+++ CW/Data/DbSeeder.cs
@@ -125,50 +125,5 @@ namespace CW.Data
                 }
             }
         }
-
-        //public static void CreateUsers(UserManager<IdentityUser> userManager)
-        //{
-        //    IdentityUser member1 = new IdentityUser
-        //    {
-        //        UserName = "Member1@email.com",
-        //        Email = "Member1@email.com",
-        //    };
-        //    userManager.CreateAsync(member1, "Password123!").Wait();
-
-        //    IdentityUser customer1 = new IdentityUser
-        //    {
-        //        UserName = "Customer1@email.com",
-        //        Email = "Customer1@email.com",
-        //    };
-        //    userManager.CreateAsync(customer1, "Password123!").Wait();
-
-        //    IdentityUser customer2 = new IdentityUser
-        //    {
-        //        UserName = "Customer2@email.com",
-        //        Email = "Customer2@email.com",
-        //    };
-        //    userManager.CreateAsync(customer2, "Password123!").Wait();
-
-        //    IdentityUser customer3 = new IdentityUser
-        //    {
-        //        UserName = "Customer3@email.com",
-        //        Email = "Customer3@email.com",
-        //    };
-        //    userManager.CreateAsync(customer3, "Password123!").Wait();
-
-        //    IdentityUser customer4 = new IdentityUser
-        //    {
-        //        UserName = "Customer4@email.com",
-        //        Email = "Customer4@email.com",
-        //    };
-        //    userManager.CreateAsync(customer4, "Password123!").Wait();
-
-        //    IdentityUser customer5 = new IdentityUser
-        //    {
-        //        UserName = "Customer5@email.com",
-        //        Email = "Customer5@email.com",
-        //    };
-        //    userManager.CreateAsync(customer5, "Password123!").Wait();
-        //}
     }
 }
